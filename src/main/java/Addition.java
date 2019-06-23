a0e53b7e8a7a663206c81f243ff313dc44ca0c18
nagasai9990 <nagasai9990@gmail.com>
2019-06-23 18:53:14 +0530

    sonar commit

---
+++ src/main/java/Addition.java
@@ -0,0 +1,20 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;b=4;
+		c=a+b;
+		
+		
+		System.out.println("the value of cis " +c);
+		
+				
+				
+				
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
